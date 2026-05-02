1f27005ebeb3c66b8ef5d273b0564434282cb0e5
Hassen Hannachi <hannachihassen07@gmail.com>
2026-05-01 23:08:12 -0400

    Edit end_and_if.py

---
+++ examples/16_end_and_if.py
@@ -0,0 +1,23 @@
+ people = 30
+ cars = 40
+ trucks = 15
+
+ if cars > people:
+     print("we should take the cars.")
+
+ elif Cars < people:
+     print("we should not take the car.")
+ else:
+     print("we can't decide.")
+
+ if trucks > cars:
+     print("Tha's too many trucks.")
+ elif trucks < cars:
+     print("Maybe we could take the trucks.")
+ else:
+     print("we still can't decide.")
+
+ if people > trucks:
+    print("Alright, let's take the trukcs.")
+ else:
+    print("Fine, let's stay home then.")
