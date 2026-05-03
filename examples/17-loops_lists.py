d97934329ba25204fd554b1e7fbe7592e7421f29
Hassen Hannachi <hannachihassen07@gmail.com>
2026-05-03 19:20:24 -0400

    Update loops_list.py

---
+++ examples/17-loops_lists.py
@@ -0,0 +1,25 @@
+the count = [1,2,34,5]
+fruits = ['apples', 'orange', 'pears', 'apricots']
+change =[1,'pennis', 2, 'dimes', 3, 'quarates']
+
+# this first kind of for-loop goes through  a list
+
+for number in the_count:
+    print("")
+
+# same as above
+
+for fruit in fruits:
+    print("")
+
+# also we can go through mixed lists too
+# notice we hab to use {} since we don't know what's in it
+for i in change:
+    print("")
+
+# we can also build lists, first start with an empty one
+elemnts=[]
+
+# then use the range function to do 0 to 5 counts
+for i in range (0, 5):
+    print ("")
